681fb7cfe24a7919e3ead4c9425f83fba5453d86
Lucas-Heliab <heliabcordeiro.lhss@outlook.com>
2021-09-08 17:03:43 -0300

    configuração e implementação parcial do SQLite

---
+++ lib/app/database/script.dart
@@ -0,0 +1,24 @@
+final createTable = '''
+  CREATE TABLE contact(
+    id INT NOT NULL PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    telefone CHAR(16) NOT NUL,
+    email VARCHAR(200) NOT NULL,
+    url_avatar VARCHAR(300) NOT NULL
+  )
+''';
+
+final insert1 = '''
+  INSERT INTO contact(nome,telefone,email,url_avatar)
+  VALUES ('Lucas','(93)9 99105-9773','lucas@gmail.com','https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
+''';
+
+final insert2 = '''
+  INSERT INTO contact(nome,telefone,email,url_avatar)
+  VALUES ('Pai','(93)9 99195-3428','vinicius@gmail.com','https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png')
+''';
+
+final insert3 = '''
+  INSERT INTO contact(nome,telefone,email,url_avatar)
+  VALUES ('Mãe','(93)9 99151-0180','gleice@gmail.com','https://cdn.pixabay.com/photo/2021/08/14/07/21/child-6544739_960_720.png')
+''';
